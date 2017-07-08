f87d5a25b09dfa29c56863917cb9a94653d5f9f2
gff1055 <gff105@gmail.com>
2017-07-08 00:32:22 -0300

    carregando arquivos javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+requirejs(["helper/util"], function(util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+    
+    /*Esta funcao é chamada quando o arquivo util.js (dentro da pasta /scripts/helper) é carregado.
+    Se util.js chama define(), entao esta funcao não é disparada até as
+    ependencias de "util" serem carregadas, e o parametro util mantera o valor do modulo para "helper/util"
+    */
+    
+    
+});
