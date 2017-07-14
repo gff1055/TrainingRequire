cf54d12a55335882aa14bc46eec2fca1e80eb722
gff1055 <gff105@gmail.com>
2017-07-13 23:38:53 -0300

    modulos no require

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 requirejs.config({
+    
+    //POR PADRAO CARREGA QUALQUER MODULO DE js/lib
+    //DEVE SER CONFIGURADO NO MESMO DIRETORIO RAIZ DO SCRIPT SETADO NO data-main
     baseUrl: 'js/lib';
+    
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    
+    //INCLUINDO PS MODULOS DA PASTA app NO CARREGAMENTO
+    paths:{
+        app:'../app'
+    }
+
+    //STARTS THE MAIN APP LOGIC
+    //INICIA A LOGICA DO APP PRINCIPAL
+    requirejs(['jquery','canvas','app/sub'],
+        function($, canvas, sub){
+            
+            //jQuery, canvas and the app/sub module are all
+            //loaded and can be used here now.
+
+            /*jQuery, canvas E O MODULO app/sub SAO TODOS CARREGADOS E PODEM SER USADOS AGORA */
+
+
+        }
+    )
+    
 })
